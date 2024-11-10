39f2828b4e2a3de7d505f01b77e92d3f85c91362
Camilo Madariaga Dozaret <cmdozaret@gmail.com>
2024-11-10 05:50:56 -0500

    feat: setting project title, routes (constants->routes) and token for development enviroment

--- src/app/constants/constant.ts
+++ src/app/constants/constant.ts
@@ -1,3 +1,18 @@
 export const Constant = {
-    API_METHOD: {}
+    API_ROUTES: {
+        USER_ROUTES: {
+            LIST: 'user',
+            DELETE: 'user',
+        },
+        AUTH_ROUTES: {
+            LOGIN: 'auth/login',
+            LOGOUT: 'auth/logout',
+        },
+        FILE_ROUTES: {
+            LIST: 'file',
+            UPLOAD: 'file',
+            DOWNLOAD: 'file/download',
+            DELETE: 'file',
+        }
+    }
 }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,7 @@
 export const environment = {
+    PROYECT_NAME: 'InstaShare',
     API_PORT: 3500,
     API_URL: `http://localhost`,
+    LOCAL_STORAGE_TKN: 'instaShare-token',
+    LOCAL_STORAGE_RTK: 'instaShare-refreshToken',
 };
